d08b0db74c5b1899bd12ffbd815ff29c1c4018d2
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-05-31 18:52:29 -0600

    cambios en el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,36 @@
+import NavSecond from "./NavSecond";
+
 const Header = () => {
   return (
-    <section className="text-center w-full">
-      <div className="flex justify-center items-center pt-3 ms:mt-14">
-        <div className="mr-10">
-          <img
-            className="w-[100px] h-[80px] transition-opacity duration-300 transform hover:translate-y-1 bg-contain translate-y-4 ms:w-[130px] ms:h-[100px]"
-            src="/images/Mi proyecto.png"
-            alt=""
-          />
-        </div>
-        <div className="text-right">
-          <h1 className="text-3xl font-bold pt-4 transition-opacity duration-300 transform hover:translate-y-1 ms:text-5xl">
-            Flor Jarquin
-          </h1>
-          <h6 className="text-xs font-light text-orange-300 pt-2 transition-opacity duration-300">
-            FlorJarquin.com
-          </h6>
-        </div>
+    <section
+      className="h-screen w-full bg-gradient-to-t
+    to-white from-black"
+    >
+      <NavSecond />
+
+      <div className="flex flex-col items-center translate-y-16 ms:text-white ms:text-6xl ">
+        <h1 className="text-4xl font-bold ms:text-6xl">Hi i′m Flor Jarquin</h1>
+        <p className="text-2xl text-center text-black m-3 sm:max-w-[700px] sm:text-3xl">
+          &ldquo;A real estate expert, she guides her clients to successful
+          investments with knowledge and confidence.&rdquo;
+        </p>
       </div>
 
-      <div id="houseinfo">
-        <svg
-          className="bottom-0 w-full -translate-y-5 transition-opacity duration-300 md:rounded-xl transform hover:translate-y-0"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-        >
-          <path
-            fill="url(#gradient)" // Reemplaza "gradient" por el nombre del gradiente que elijas
-            d="M0,160L48,176C96,192,192,224,288,224C384,224,480,192,576,192C672,192,768,224,864,208C960,192,1056,128,1152,112C1248,96,1344,128,1392,144L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-          ></path>
-          <defs>
-            <linearGradient id="gradient" gradientTransform="rotate(90)">
-              <stop offset="0%" stopColor="#FF0080" />
-              <stop offset="100%" stopColor="#FF8C00" />
-            </linearGradient>
-          </defs>
-        </svg>
+      <div className="translate-y-10 sm:translate-y-20 ">
+        <img
+          id="moving-image"
+          className="w-[100px] absolute right-[1px] sm:right-32 ms:w-[150px] "
+          src="/public/images/book.png"
+          alt=""
+        />
+      </div>
+
+      <div className="ms:pt-5">
+        <img
+          className="absolute bottom-0 right-1/2 transform translate-x-1/2 ms:w-[330px]"
+          src="/public/images/flor.png"
+          alt=""
+        />
       </div>
     </section>
   );
---
+++ src/components/NavSecond.jsx
@@ -0,0 +1,30 @@
+import { Container, Navbar } from "react-bootstrap";
+
+const NavSecond = () => {
+  return (
+    <Navbar className=" md:mt-14 w-full backdrop-blur-sm" variant="dark">
+      <Container className="flex justify-content-between items-center">
+        <Navbar.Brand
+          className="flex items-center justify-content-between"
+          href="#home"
+        >
+          <img
+            alt=""
+            src="/public/images/Mi proyecto.png"
+            width="70"
+            height="70"
+            className="d-inline-block align-top"
+          />
+          <h3
+            id="moving-heading"
+            className="text-black font-bold sm:text-3xl sm:text-red-700"
+          >
+            Costa Rica Real State
+          </h3>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavSecond;
